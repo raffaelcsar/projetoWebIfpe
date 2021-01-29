b1b32c81b3bdb1fbdcd52e4de021c93b7e406054
Victor-Raphael <56162733+victoorraphael@users.noreply.github.com>
2021-01-29 15:27:26 -0300

    versao mobile

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const path = require('path')
 
 const app = express()
 
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html')
+    res.sendFile(__dirname + '/public/views/index.html')
 })
 
 app.listen(3000, () => console.log('listening on port: ' + 3000))
